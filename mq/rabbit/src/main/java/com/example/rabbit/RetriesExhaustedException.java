656a984b9b8f4f56b7f9b3c92570d08241d3cf0c
hai <hilin2333@gmail.com>
2022-02-16 16:12:07 +0800

    message获取

--- mq/rabbit/src/main/java/com/example/rabbit/RetriesExhaustedException.java
+++ mq/rabbit/src/main/java/com/example/rabbit/RetriesExhaustedException.java
@@ -2,6 +2,6 @@ package com.example.rabbit;
 
 public class RetriesExhaustedException extends RuntimeException{
     public RetriesExhaustedException(Throwable cause) {
-        super(cause.getMessage());
+        super(cause.getCause() != null ? cause.getCause().getMessage() : cause.getMessage());
     }
 }
